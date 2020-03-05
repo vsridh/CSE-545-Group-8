5b221506123731e8efd9611013bf505babcef32e
vijaiharihar <vijaiharihar@gmail.com>
2020-03-05 16:35:51 -0700

    .gitignore fix

--- group_project/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/vijai/7846CE1305CB113B/MS-files/sem2/soft-sec/CSE-545-Group-8/group_project/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
