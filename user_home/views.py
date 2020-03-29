dac578adb8176e9b4b5256532ba01600cb60f414
akshay4241 <akshay@NAkshay-MacBook-Pro.local>
2020-03-28 18:49:06 -0700

    authentication for each function added

--- user_home/views.py
+++ user_home/views.py
@@ -14,80 +14,91 @@ from django.views.generic.edit import UpdateView
 from home import models
 
 # Create your views here.
-def user_home(request):
-	if request.user.is_authenticated:
+def user_home(request):      
+    profile_instance = models.Profile.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_active and profile_instance.privilege_id.user_type=="Customer":
 		# return HttpResponse('Session established')
-		return render(request, 'user_homepage.html', {'username':request.user.username})
-	else:
-		return HttpResponse('Try again!')
+        return render(request, 'user_homepage.html', {'username':request.user.username})
+    else:
+        return HttpResponse('Try again!')
 
 def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect('/')
 
 def appointment(request):
-    if request.method == 'POST':
-        form = AppointmentForm(request.POST)
-        if form.is_valid():
-            print("yo2")
-            app=form.save(commit=False)
-            app.user=request.user
-            app.save()
-            if request.user.is_authenticated and request.user.is_active:
+    profile_instance = models.Profile.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_active and profile_instance.privilege_id.user_type=="Customer":
+        if request.method == 'POST':
+            form = AppointmentForm(request.POST)
+            if form.is_valid():
+                app=form.save(commit=False)
+                app.user=request.user
+                app.save()
                 return HttpResponseRedirect('/user_home/')
             else:
-                return HttpResponse("Login Failed!! Wrong username or password")
+                return HttpResponse("invalid form")
+        else:
+            form=AppointmentForm()
+        context={'form' : form}
+        return render(request,'Appointment/appointment.html',context)
     else:
-        form=AppointmentForm()
-    context={'form' : form}
-    return render(request,'Appointment/appointment.html',context)
+        return HttpResponse("Login Failed!! Wrong username or password")
 
 def newAccount(request):
-    if request.method == 'POST':
-        acc_form = AccountForm(request.POST)
-        if acc_form.is_valid():
-            a=acc_form.save(commit=False)
-            a.user=request.user
-            a.save()
-            if request.user.is_authenticated and request.user.is_active:
+    profile_instance = models.Profile.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_active and profile_instance.privilege_id.user_type=="Customer":
+        if request.method == 'POST':
+            acc_form = AccountForm(request.POST)
+            if acc_form.is_valid():
+                a=acc_form.save(commit=False)
+                a.user=request.user
+                a.save()
                 return HttpResponseRedirect('/user_home/')
             else:
-                return HttpResponse("Login Failed!! Wrong username or password")
+                return HttpResponse("invalid form")
+        else:
+            acc_form = AccountForm()
+        context={'acc_form' : acc_form}
+        return render(request,'new_account/new_account.html',context)
     else:
-        acc_form = AccountForm()
-    context={'acc_form' : acc_form}
-    return render(request,'new_account/new_account.html',context)
+        return HttpResponse("Login Failed!! Wrong username or password")
 
 def deleteAccount(request):
-    if request.method == 'POST':
-        acc_form = AccountDeleteForm(request.POST)
-        if acc_form.is_valid():
-            acc_number=acc_form.cleaned_data
-            account_instance = models.Account.objects.get(account_number=acc_number.get('account_number'))
-            account_instance.delete=True
-            account_instance.save()
-            if request.user.is_authenticated and request.user.is_active:
+    profile_instance = models.Profile.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_active and profile_instance.privilege_id.user_type=="Customer":
+        if request.method == 'POST':
+            acc_form = AccountDeleteForm(request.POST)
+            if acc_form.is_valid():
+                acc_number=acc_form.cleaned_data
+                account_instance = models.Account.objects.get(account_number=acc_number.get('account_number'))
+                account_instance.delete=True
+                account_instance.save()
                 return HttpResponseRedirect('/user_home/')
             else:
-                return HttpResponse("Login Failed!! Wrong username or password")
+                return HttpResponse("invalid form")
+        else:
+            acc_form = AccountDeleteForm()
+        context={'acc_form' : acc_form}
+        return render(request,'delete_account/delete_account.html',context)
     else:
-        acc_form = AccountDeleteForm()
-    context={'acc_form' : acc_form}
-    return render(request,'delete_account/delete_account.html',context)
+        return HttpResponse("Login Failed!! Wrong username or password")
 
 def updateProfile(request):
-    if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST)
-        if user_form.is_valid():
-            u=user_form.save(commit=False)
-            u.user=request.user
-            u.save()
-            if request.user.is_authenticated and request.user.is_active:
+    profile_instance = models.Profile.objects.get(user=request.user)
+    if request.user.is_authenticated and request.user.is_active and profile_instance.privilege_id.user_type=="Customer":
+        if request.method == 'POST':
+            user_form = UserUpdateForm(request.POST)
+            if user_form.is_valid():
+                u=user_form.save(commit=False)
+                u.user=request.user
+                u.save()
                 return HttpResponseRedirect('/user_home/')
             else:
-                return HttpResponse("Login Failed!! Wrong username or password")
+                return HttpResponse("invalid form")
+        else:
+            user_form = UserUpdateForm()
+        context={'user_form' : user_form}
+        return render(request,'profile_update/profile_update.html',context)
     else:
-        user_form = UserUpdateForm()
-
-    context={'user_form' : user_form}
-    return render(request,'profile_update/profile_update.html',context)
+            return HttpResponse("Login Failed!! Wrong username or password")
