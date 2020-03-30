bf803ba915942991a23bec87396f08ccdaec2883
akshay4241 <akshay@NAkshay-MacBook-Pro.local>
2020-03-30 07:31:03 -0700

    added otp to login

--- login/views.py
+++ login/views.py
@@ -5,9 +5,18 @@ from django.contrib.auth import authenticate, login
 from datetime import datetime
 from create_account import forms
 from home.models import User,Profile
-from .forms import LoginForm
-from .forms import LoginForm
+from .forms import LoginForm,Otp,Token
 import time
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_text
+from random import randint
+from django.urls import reverse
+
 
 #try wrong account list ------ username: number of try
 block_wait_list={}
@@ -24,6 +33,10 @@ def login_user(request):
 
         if form.is_valid():
             userObj = authenticate(username=form.cleaned_data['user_id'], password=form.cleaned_data['password'])
+            request.session['username'] = form.cleaned_data['user_id']
+            request.session['password'] = form.cleaned_data['password']
+            user_instance=User.objects.get(username=form.cleaned_data['user_id'])
+            profile_instance=Profile.objects.get(user=user_instance)
             #check block list
             if form.cleaned_data['user_id'] in block_list:
                 #check block time
@@ -33,21 +46,13 @@ def login_user(request):
                     #return to block page
                     return login_block(request)
 
-            #login
-            user_instance=User.objects.get(username=form.cleaned_data['user_id'])
-            profile_instance=Profile.objects.get(user=user_instance)
-            if userObj is not None and profile_instance.flag==True and user_instance.is_active:
-                login(request,userObj)
-                request.session['last_activity'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-
-                #delete user in block list
+            #login#delete user in block list
                 if form.cleaned_data['user_id'] in block_list:
                     block_list.pop(form.cleaned_data['user_id'])
                 if form.cleaned_data['user_id'] in block_wait_list:
                     block_wait_list.pop(form.cleaned_data['user_id'])
                 #return user home page
-                return HttpResponseRedirect('/user_home/')
-            else:
+            elif userObj is None or profile_instance.flag==False or user_instance.is_active==False:
                 #not in block list add
                 if form.cleaned_data['user_id'] in block_wait_list:
                     try_times=block_wait_list[form.cleaned_data['user_id']]
@@ -62,11 +67,23 @@ def login_user(request):
                     block_list[form.cleaned_data['user_id']]=start_time
                     block_wait_list.pop(form.cleaned_data['user_id'])
                     return login_block(request)
-
-                return HttpResponse("Login Failed!! Wrong username or password"+str(try_times+1))
             #if authorization is successful, redirect to appropriate page
             #return HttpResponseRedirect('/')
-
+            elif userObj is not None and profile_instance.flag==True and user_instance.is_active:
+                token=randint(10000,99999)
+                to_email=user_instance.email
+                mail_subject = 'Login OTP'
+                message = render_to_string('authenticate_otp.html', {
+                'user': request.user,
+                'otp' : token
+                 })
+                email = EmailMessage(
+                            mail_subject,message, to=[to_email]
+                )
+                email.send()
+                request.method='GET'
+                request.session['token'] = token
+                return verify_otp(request)
     else:
         form = LoginForm()
 
@@ -74,6 +91,24 @@ def login_user(request):
 
 def login_block(request):
     return HttpResponse("Login block!!need wait for")
+
+def verify_otp(request):
+    if request.method == 'POST':
+        form = Otp(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            print(form.cleaned_data)
+            if form.cleaned_data['otp'] == request.session['token']:
+                userObj = authenticate(username=request.session['username'], password=request.session['password'])
+                login(request,userObj)
+                request.session['last_activity'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                return HttpResponseRedirect('http://127.0.0.1:8000/user_home')
+        return HttpResponse("Login Failed!! Wrong OTP")
+    else:
+        form = Otp()
+    context={'form' : form}
+    return render(request,'enter_otp.html',context)
+
 
 
 
